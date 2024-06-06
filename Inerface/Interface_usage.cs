cc5e6fe78c301e2590f01f0c538ccf15b3794756
FantasWolf <jaganhari2002@gmail.com>
2024-06-06 20:50:45 +0530

    Added Interface

---
+++ Inerface/Interface_usage.cs
@@ -0,0 +1,50 @@
+using System;
+
+interface MyInterface
+{
+    void Functionality();
+}
+
+class Pencil:MyInterface
+{
+    public void Functionality()
+    {
+        Console.WriteLine("Writing the words");
+    }
+}
+
+class Eraser : MyInterface
+{
+    public void Functionality()
+    {
+        Console.WriteLine("Erasing the words");
+    }
+}
+
+
+class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    MyInterface Interfacel;
+    public Student (MyInterface interface_obj)
+    {
+        Interfacel = interface_obj;
+    }
+
+    public void TaskMethod()
+    {
+        Interfacel.Functionality();
+    }
+}
+
+class Test
+{
+    public static void Main(String[] args)
+    {
+        MyInterface passiing_func = new Eraser();
+        Student student = new Student(passiing_func);
+        student.TaskMethod();
+    }
+}
